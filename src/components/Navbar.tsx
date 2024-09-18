23dc7c4a446a55f3dde130cf6c6e2850d87d82a1
Muhammad Elang Hardifal <muelava@gmail.com>
2024-09-18 16:01:24 +0700

    three commits

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import "@fontsource/poppins";
 
 const Navbar: React.FC = () => {
     return (
-        <nav>
-            <ul className='flex fixed justify-between w-full'>
+        <nav className='fixed w-full py-5 bg-neutral-800 text-slate-100' style={{ fontFamily: 'Poppins' }}>
+            <ul className='flex'>
                 <li>
                     <Link to="/">Home</Link>
                 </li>

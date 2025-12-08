ca3bd02ed4640a4e4ad19324f085c2c9c8c5ede3
Elang <muelava@gmail.com>
2025-12-08 14:47:56 +0700

    Refactor code structure and remove redundant code blocks for improved readability and maintainability

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,10 +6,13 @@ import { RiExternalLinkFill, RiInstagramFill, RiNextjsFill, RiTailwindCssFill } 
 import { SiShowwcase } from "react-icons/si";
 import { IoLogoIonic, IoLogoJavascript } from "react-icons/io";
 import { BiLogoTypescript } from "react-icons/bi";
-import { IoCodeSlashOutline, IoLogoCapacitor } from "react-icons/io5";
+import {
+  // IoCodeSlashOutline,
+  IoLogoCapacitor,
+} from "react-icons/io5";
 import { TbBrandReactNative } from "react-icons/tb";
 import { portofolio } from "../utils/constants";
-import { HiOutlineLink } from "react-icons/hi";
+// import { HiOutlineLink } from "react-icons/hi";
 import SplitText from "../components/Splite";
 import GradientText from "../components/GradientText";
 import { Eye } from "lucide-react";
@@ -176,19 +179,19 @@ const Home: React.FC<HomeProps> = ({ sectionRefs }) => {
         <div className="grid grid-cols-1 md:grid-cols-3 gap-5 max-w-7xl mx-auto">
           {data.map((item: any) => (
             <div key={item.id}>
-              <div className="w-full h-56 rounded overflow-hidden group relative hover:scale-105 transition-all duration-300" style={{ backgroundImage: `url(${item.thumbnails})`, backgroundSize: "cover", backgroundRepeat: "no-repeat", backgroundPosition: "center" }}>
-                <div className="w-ful h-full flex flex-col pb-4 bg-neutral-950/60 group-hover:bg-neutral-950/80 transition-all duration-300 group-hover:text-slate-200/50">
-                  <h6 className="font-bold shadow-xl text-lg rounded border-l-4 border-teal-300 ps-3 uppercase mb-2">{item.title}</h6>
-                  <p className="opacity-80 text-sm ms-4">{item?.description}</p>
-                  <div className="flex gap-3 ms-4 mt-auto flex-wrap">
+              <div className="w-full h-56 rounded-xl overflow-hidden bg-teal-800/20 hover:bg-teal-800/40 group relative hover:scale-105 transition-all duration-300" style={{ backgroundImage: `url(${item.thumbnails})`, backgroundSize: "cover", backgroundRepeat: "no-repeat", backgroundPosition: "center" }}>
+                <div className="w-ful h-full flex flex-col pb-4 transition-all duration-300 group-hover:text-slate-200/50">
+                  {/* <h6 className="font-bold text-lg rounded border-l-4 border-teal-300 ps-3 uppercase mb-2">{item.title}</h6> */}
+                  {/* <p className="opacity-80 text-sm ms-4">{item?.description}</p> */}
+                  {/* <div className="flex gap-3 ms-4 mt-auto flex-wrap">
                     {item.tech?.map((item: any, i: number) => (
                       <span key={i} className="text-xs px-1.5 rounded-full py-0.5 font-semibold border border-dashed text-slate-300/60 border-slate-400/50">
                         {item}
                       </span>
                     ))}
-                  </div>
+                  </div> */}
                 </div>
-                <button className="absolute top-0 right-0 bottom-0 left-0 hidden group-hover:flex gap-3 justify-center items-center">
+                {/* <button className="absolute top-0 right-0 bottom-0 left-0 hidden group-hover:flex gap-3 justify-center items-center">
                   {item.link?.website && (
                     <Link to={item.link?.website} target="_blank" className="border inline-block border-teal-500 hover:scale-110 hover:bg-teal-500 transition-all p-2 text-teal-500 hover:text-slate-200 rounded-full">
                       <HiOutlineLink size={28} />
@@ -204,7 +207,7 @@ const Home: React.FC<HomeProps> = ({ sectionRefs }) => {
                       <IoCodeSlashOutline size={28} />
                     </Link>
                   )}
-                </button>
+                </button> */}
               </div>
             </div>
           ))}
--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -1,77 +1,88 @@
-
 const portofolio = () => [
-    {
-        id: 1,
-        thumbnails: "/assets/img/satu-ilmu.png",
-        title: "Satu Ilmu",
-        description: "Company Profile, Education Technology",
-        link: {
-            website: "https://satuilmu.com/",
-            design: "https://www.figma.com/proto/eXX7gIVdvnK7M725x8pzBg/satu_ilmu_web?node-id=460-337&node-type=canvas&t=SNw5443kIX8pURg5-0&scaling=scale-down-width&content-scaling=fixed&page-id=460%3A336&starting-point-node-id=460%3A337",
-            source: "https://github.com/muelava/satuilmu",
-        },
-        tech: ["Figma", "Javascript", "Tailwind Css", "Codeigniter"]
+  {
+    id: 1,
+    thumbnails: "/assets/img/project-satu-ilmu.png",
+    title: "Satu Ilmu",
+    description: "Company Profile, Education Technology",
+    link: {
+      website: "https://satuilmu.com/",
+      design: "https://www.figma.com/proto/eXX7gIVdvnK7M725x8pzBg/satu_ilmu_web?node-id=460-337&node-type=canvas&t=SNw5443kIX8pURg5-0&scaling=scale-down-width&content-scaling=fixed&page-id=460%3A336&starting-point-node-id=460%3A337",
+      source: "https://github.com/muelava/satuilmu",
     },
-    {
-        id: 2,
-        thumbnails: "/assets/img/ruang-nyaman.png",
-        title: "Ruang Nyaman",
-        description: "Company Profile, CMS",
-        link: {
-            website: "https://ruangnyaman.design/",
-            design: null,
-            source: "https://github.com/muelava/ruangnyaman",
-        },
-        tech: ["Figma", "Javascript", "Tailwind Css", "Laravel"]
+    tech: ["Figma", "Javascript", "Tailwind Css", "Codeigniter"],
+  },
+  // {
+  //   id: 2,
+  //   thumbnails: "/assets/img/ruang-nyaman.png",
+  //   title: "Ruang Nyaman",
+  //   description: "Company Profile, CMS",
+  //   link: {
+  //     website: "https://ruangnyaman.design/",
+  //     design: null,
+  //     source: "https://github.com/muelava/ruangnyaman",
+  //   },
+  //   tech: ["Figma", "Javascript", "Tailwind Css", "Laravel"],
+  // },
+  {
+    id: 3,
+    thumbnails: "/assets/img/project-danamart-id.png",
+    title: "Danamart Id",
+    description: "Company Profile, Web Application",
+    link: {
+      website: "https://danamart.id/",
+      design: null,
+      source: "https://github.com/muelava/danamart",
     },
-    {
-        id: 3,
-        thumbnails: "/assets/img/danamart-id.png",
-        title: "Danamart Id",
-        description: "Company Profile, Web Application",
-        link: {
-            website: "https://danamart.id/",
-            design: null,
-            source: "https://github.com/muelava/danamart",
-        },
-        tech: ["Figma", "Javascript", "Tailwind Css", "Webpack"]
+    tech: ["Figma", "Javascript", "Tailwind Css", "Webpack"],
+  },
+  {
+    id: 4,
+    thumbnails: "/assets/img/diggo.png",
+    title: "Diggo",
+    description: "Company Profile, Point Of Sale (POS)",
+    link: {
+      website: "https://demo.diggo.id/",
+      design: "https://www.figma.com/proto/9gJhWsDFkXZtLRAgbpdR5g/aplikasi-diggo-(Copy)?node-id=3278-10228&node-type=frame&t=3kG1d0yOAO7yBD3F-1&scaling=scale-down&content-scaling=fixed&page-id=13%3A35365&starting-point-node-id=3278%3A10228&show-proto-sidebar=1",
+      source: "https://github.com/muelava/diggo-landing-page",
     },
-    {
-        id: 4,
-        thumbnails: "/assets/img/diggo.png",
-        title: "Diggo",
-        description: "Company Profile, Point Of Sale (POS)",
-        link: {
-            website: "https://demo.diggo.id/",
-            design: "https://www.figma.com/proto/9gJhWsDFkXZtLRAgbpdR5g/aplikasi-diggo-(Copy)?node-id=3278-10228&node-type=frame&t=3kG1d0yOAO7yBD3F-1&scaling=scale-down&content-scaling=fixed&page-id=13%3A35365&starting-point-node-id=3278%3A10228&show-proto-sidebar=1",
-            source: "https://github.com/muelava/diggo-landing-page",
-        },
-        tech: ["Figma", "React", "TailwindCss", "Laravel"]
+    tech: ["Figma", "React", "TailwindCss", "Laravel"],
+  },
+  {
+    id: 5,
+    thumbnails: "/assets/img/project-prokoin.png",
+    title: "Prokoin",
+    description: "Tech Development Services",
+    link: {
+      website: "https://www.prokoin.com/",
+      design: "https://www.figma.com/proto/MsU4n9rM0jQ3CAGyTz4OSu/Prokoin-ID?node-id=12-777&starting-point-node-id=12%3A777&t=dcgri6gH5rMKVxsk-1",
+      source: null,
     },
-    {
-        id: 5,
-        thumbnails: "/assets/img/prokoin.png",
-        title: "Prokoin",
-        description: "Tech Development Services",
-        link: {
-            website: "https://www.prokoin.com/",
-            design: "https://www.figma.com/proto/MsU4n9rM0jQ3CAGyTz4OSu/Prokoin-ID?node-id=12-777&starting-point-node-id=12%3A777&t=dcgri6gH5rMKVxsk-1",
-            source: null,
-        },
-        tech: ["Figma", "TailwindCss", "React", "Next", "NextUI"]
+    tech: ["Figma", "TailwindCss", "React", "Next", "NextUI"],
+  },
+  {
+    id: 6,
+    thumbnails: "/assets/img/project-danamart-mobile.png",
+    title: "Danamart Mobile Apps",
+    description: "Mobile, Financial Technology",
+    link: {
+      website: "https://danamart.id/",
+      design: "https://www.figma.com/proto/S2kfBFxWs0Ix8Dzxq2bGnH/special-case?node-id=919-671&node-type=canvas&t=q4xlsimVGKO5nu9K-1&scaling=scale-down&content-scaling=fixed&page-id=511%3A111&starting-point-node-id=919%3A671&show-proto-sidebar=1",
+      source: null,
     },
-    {
-        id: 6,
-        thumbnails: "/assets/img/danamart-mobile.png",
-        title: "Danamart Mobile Apps",
-        description: "Mobile, Financial Technology",
-        link: {
-            website: "https://danamart.id/",
-            design: "https://www.figma.com/proto/S2kfBFxWs0Ix8Dzxq2bGnH/special-case?node-id=919-671&node-type=canvas&t=q4xlsimVGKO5nu9K-1&scaling=scale-down&content-scaling=fixed&page-id=511%3A111&starting-point-node-id=919%3A671&show-proto-sidebar=1",
-            source: null,
-        },
-        tech: ["Figma", "Ionic", "Typescript", "React", "Tailwind Css"]
+    tech: ["Figma", "Ionic", "Typescript", "React", "Tailwind Css"],
+  },
+  {
+    id: 6,
+    thumbnails: "/assets/img/project-prowedding.png",
+    title: "Prowedding",
+    description: "Wedding Organizer",
+    link: {
+      website: "https://prowedding.id/",
+      design: "https://prowedding.id/",
+      source: null,
     },
-]
+    tech: ["Figma", "Ionic", "Typescript", "React", "Tailwind Css"],
+  },
+];
 
-export { portofolio }
+export { portofolio };
